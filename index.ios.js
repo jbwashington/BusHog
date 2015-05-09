651aaba413b72987088d46484b92a234bcb128a6
James Washington <jbwashington@gmail.com>
2015-05-08 19:09:34 -0500

    changed title color

--- index.ios.js
+++ index.ios.js
@@ -3,118 +3,120 @@
 var React = require('react-native');
 
 var {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    ScrollView
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView
 } = React;
 
+// imports api module
 var AppData = require('./AppData.js');
 
+
 var BusHog = React.createClass({
-    getInitialState: function getInitialState() {
-        return {
-            busse: []
-        };
-    },
-    componentDidMount: function componentDidMount() {
-        setInterval(() => {
-            AppData.busse().then(
-                (responseData) => {
-                    this.setState({
-                        busse: responseData.vehicles
-                    });
-                }
-            )
-        }, 500);
-    },
-    render: function() {
-        var busse = this.state.busse;
-        busse = busse.map((item) => {
-            return (
-                <View style={styles.busContainer}>
-                    <Text style={styles.busLine}>{item.line}</Text>
-                    <View style={styles.details}>
-                        <Text style={styles.smallTitle}>FROM</Text>
-                        <Text style={styles.mediumTitle}>{item.origin.toUpperCase()}</Text>
-                        <Text style={styles.smallTitle}>TO</Text>
-                        <Text style={styles.mediumTitle}>{item.destination.toUpperCase()}</Text>
+  getInitialState: function getInitialState() {
+    return {
+      busse: []
+    };
+  },
+  componentDidMount: function componentDidMount() {
+    setInterval(() => {
+      AppData.busse().then(
+        (responseData) => {
+          this.setState({
+            busse: responseData.vehicles
+          });
+        }
+      )
+    }, 500);
+  },
+  render: function() {
+    var busse = this.state.busse;
+    busse = busse.map((item) => {
+      return (
+        <View style={styles.busContainer}>
+        <Text style={styles.busLine}>{item.line}</Text>
+        <View style={styles.details}>
+        <Text style={styles.smallTitle}>FROM</Text>
+        <Text style={styles.mediumTitle}>{item.origin.toUpperCase()}</Text>
+        <Text style={styles.smallTitle}>TO</Text>
+        <Text style={styles.mediumTitle}>{item.destination.toUpperCase()}</Text>
 
-                        <Text style={styles.location}>
-                            φ {item.latitude} λ {item.longitude}
-                        </Text>
-                    </View>
-                </View>
-            );
-        })
+        <Text style={styles.location}>
+        φ {item.latitude} λ {item.longitude}
+        </Text>
+        </View>
+        </View>
+      );
+    })
 
-        return (
-            <View style={styles.main}>
-                <Text style={styles.title}>bus hog</Text>
-                <ScrollView>
-                    <View style={styles.container}>
-                        {busse}
-                    </View>
-                </ScrollView>
-            </View>
-        );
-    }
+    return (
+      <View style={styles.main}>
+      <Text style={styles.title}>bus hog</Text>
+      <ScrollView>
+      <View style={styles.container}>
+      {busse}
+      </View>
+      </ScrollView>
+      </View>
+    );
+  }
 });
 
 var styles = StyleSheet.create({
-    main: {
-        flex: 1
-    },
-    title: {
-        margin: 40,
-        fontSize: 30,
-        fontWeight: "200",
-        color: "white",
-        textAlign: 'center'
-    },
-    container: {
-        backgroundColor: 'rgb(255,255,255)'
-    },
-    busContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        borderBottomWidth: 1,
-        borderBottomColor: 'rgba(100,100,100,0.3)',
-        paddingBottom: 10,
-        paddingTop: 10
-    },
-    busLine: {
-        fontWeight: "900",
-        fontSize: 30,
-        textAlign: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        flex: 2
-    },
-    details: {
-        flex: 5,
-        paddingTop: 20,
-        paddingBottom: 15
-    },
-    smallTitle: {
-        fontSize: 10,
-        fontWeight: "200"
-    },
-    mediumTitle: {
-        fontSize: 15,
-        fontWeight: "400",
-        paddingTop: 5,
-        paddingBottom: 5
-    },
-    bold: {
-        fontWeight: "bold"
-    },
-    location: {
-        flexDirection: 'row',
-        fontSize: 12,
-        fontWeight: '200'
-    }
+  main: {
+    flex: 1
+  },
+  title: {
+    margin: 40,
+    fontSize: 30,
+    fontWeight: "200",
+    color: "black",
+    textAlign: 'center'
+  },
+  container: {
+    backgroundColor: 'rgb(255,255,255)'
+  },
+  busContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(100,100,100,0.3)',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  busLine: {
+    fontWeight: "900",
+    fontSize: 40,
+    textAlign: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    flex: 2
+  },
+  details: {
+    flex: 5,
+    paddingTop: 20,
+    paddingBottom: 15
+  },
+  smallTitle: {
+    fontSize: 10,
+    fontWeight: "200"
+  },
+  mediumTitle: {
+    fontSize: 15,
+    fontWeight: "400",
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+  bold: {
+    fontWeight: "bold"
+  },
+  location: {
+    flexDirection: 'row',
+    fontSize: 12,
+    fontWeight: '200'
+  }
 });
 
 AppRegistry.registerComponent('BusHog', () => BusHog);
